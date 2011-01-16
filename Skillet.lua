4377dc5cc1d02ed5d204f89a1bcea34ec84658b6
yossa <yossa@9913632a-c86c-4618-ab37-918a246e8810>
2011-01-16 13:37:00 +0000

    Skillet: - fix NPE

--- Skillet.lua
+++ Skillet.lua
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ]]--
 
-local MAJOR_VERSION = "2.16"
+local MAJOR_VERSION = "2.17"
 local MINOR_VERSION = ("$Revision$"):match("%d+") or 1
 local DATE = string.gsub("$Date$", "^.-(%d%d%d%d%-%d%d%-%d%d).-$", "%1")
 
