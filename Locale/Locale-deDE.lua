08873810a5c1d1c0aec20c1b3d788c6bfa9236f1
bsmorgan <bsmorgan@9913632a-c86c-4618-ab37-918a246e8810>
2015-06-12 20:08:15 +0000

    Skillet: - Cosmetic updates

--- Locale/Locale-deDE.lua
+++ Locale/Locale-deDE.lua
@@ -1,7 +1,5 @@
 --[[
- 
-Skillet: A tradeskill window replacement.
-Copyright (c) 2007 Robert Clark <nogudnik@gmail.com>
+ Skillet: A tradeskill window replacement.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,7 +13,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ]]--
 
 -- German localizations provided by Farook (from wowinterface.com) and Stan (from wowace)
