3809b080d6c24f3ba00780a752d221c412538753
bsmorgan <bsmorgan@9913632a-c86c-4618-ab37-918a246e8810>
2016-09-12 18:27:47 +0000

    Skillet: - Update TradeSkillMaster plugin

--- trunk/Plugins/TradeSkillMaster.lua
+++ trunk/Plugins/TradeSkillMaster.lua
@@ -57,8 +57,6 @@ end
 function plugin.OnEnable()
 	plugin.TSM = LibStub("AceAddon-3.0"):GetAddon("TSM_Crafting", true)
 	--DA.DEBUG(0,"plugin.TSM= "..tostring(plugin.TSM))
-	plugin.TSMA = LibStub("AceAddon-3.0"):GetAddon("TSM_AuctionDB", true)
-	--DA.DEBUG(0,"plugin.TSMA= "..tostring(plugin.TSMA))
 	if plugin.TSM and plugin.TSM.CraftingGUI then
 		plugin.GUI = plugin.TSM.CraftingGUI
 		plugin.ShowProfessionWindow = plugin.GUI.ShowProfessionWindow
@@ -71,13 +69,13 @@ function plugin.GetExtraText(skill, recipe)
 	local bop
 	if not skill or not recipe then return end
 	local itemID = recipe.itemID
-	if plugin.TSMA and plugin.TSMA.GetRealmItemData and Skillet.db.profile.plugins.TSM.enabled and itemID then
+	if TSMAPI and TSMAPI.GetItemValue and Skillet.db.profile.plugins.TSM.enabled and itemID then
 		local abacus = LibStub("LibAbacus-3.0")
-		local value = plugin.TSMA:GetRealmItemData("i:".. ItemId,  "marketValue")
+		local value = TSMAPI:GetItemValue(itemID, "DBMarket")
 		if value then
 			extra_text = abacus:FormatMoneyFull(value, true);
-			label = "Market Value"..":"
---			label = L["Market"]..":"
+			label = "DBMarket"..":"
+--			label = L["DBMarket"]..":"
 		end
 	end
 	return label, extra_text
@@ -94,9 +92,9 @@ function plugin.RecipeNameSuffix(skill, recipe)
 	local text
 	if recipe then
 		local itemID = recipe.itemID
-		if plugin.TSMA and plugin.TSMA.GetRealmItemData and Skillet.db.profile.plugins.TSM.enabled and itemID then
+		if TSMAPI and TSMAPI.GetItemValue and Skillet.db.profile.plugins.TSM.enabled and itemID then
 			local abacus = LibStub("LibAbacus-3.0")
-			local value = plugin.TSMA:GetRealmItemData("i:".. ItemId,  "marketValue")
+			local value = TSMAPI:GetItemValue(itemID, "DBMarket")
 			if value then
 				text = abacus:FormatMoneyFull(value, true);
 			end
