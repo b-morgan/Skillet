61c6e1f2f3e263fcf96433b835c95c9bcec26302
Matthew Hively <matthewhively@viz.com>
2021-07-26 12:26:52 -0700

    Added WIP ScrapeWowhead
    This appears to be working correctly, but wowhead tables are missing orange skill level most of the time. So this is currently incomplete at best.

---
+++ ScrapeWowhead.js
@@ -0,0 +1,133 @@
+// WIP
+
+// [Paste] into console for each profession page
+recipes = {}; // spellID => skill_levels
+counter = {}; // just keep track of keys
+
+is_enchanting = (document.location.pathname == '/enchanting');
+
+function get_skill_levels() {
+    $('div.listview > div:visible .listview-row').each(function(i, row){
+        // Is always the item link except for enchants where it is also the spell
+        var $first_link  = $(row).find('td:first-child a');
+        // Is always the spell link for all professions
+        var $second_link = $(row).find('td:nth-child(2) a');
+
+        var spellID = $second_link[0].href.match(/spell=[0-9]*/)[0].split('=')[1];
+        var itemID  = 0; // placeholder for enchanting
+
+        // Skip basic campfire, not a real recipe
+        //if (spellID == 818) { return; }
+
+        if (!is_enchanting) {
+            itemID = $first_link[0].href.match(/item=[0-9]*/)[0].split('=')[1];
+        }
+        // Make the ids integers
+        spellID = +spellID;
+        itemID  = +itemID;
+
+        var $td_div = $(row).find('td:nth-child(5) div');
+        // if it doesn't have skill level information, skip it, its an invalid recipe
+        if ($td_div.length == 1) {
+            console.log('skipping spellID: ' + spellID + ' - ' + $second_link.text());
+            return; // next in loop
+        }
+        var arr = [];
+        $td_div.last().find('span').each(function(j, span){
+            arr.push($(span).text());
+        });
+
+        // ---- recipe manual fixes ----
+        // TODO: if we ever find anything that needs to be tweaked
+        // ---- ----
+
+        // Almost all skills in wowhead only have 3 numbers shown
+        // Dunno if this is a bug or not, but pad them to 4
+        if (arr.length < 4) {
+          // prefill the array with duplicates of the first value
+          arr = Array(4 - arr.length).fill(arr[0]).concat(arr);
+        }
+
+        recipes[itemID] = recipes[itemID] || {}; // Maybe init
+        recipes[itemID][spellID] = arr;
+        // REM: all enchanting will be shoved under itemID(0)
+
+        counter[spellID] = 1; // Keep track of how many valid recipes we've found
+    });
+    console.log('number recipes: ' + Object.keys(counter).length);
+}
+
+function print_skill_levels() {
+    var arr = [];
+
+    if (is_enchanting) {
+        recipes = recipes[0]; // bust out of the placeholder itemID
+        $.each(recipes, function(spellID, val){
+            // [-spellID] = "A/B/C/D",  (enchanting only)
+            arr.push('[-' + spellID + '] = "' + val.join('/') + '"');
+        });
+    } else {
+        $.each(recipes, function(itemID, spells){
+            if (Object.keys(spells).length == 1) { // 99% of cases
+                var val = Object.values(spells)[0];
+                // Don't use a nested table when only one spell per item
+                // [itemID] = "A/B/C/D",
+                arr.push('[' + itemID + '] = "' + val.join('/') + '"');
+                return;
+            }
+            // [itemID] = { [spellID] = "A/B/C/D", [spellID] = "A/B/C/D" },
+            var inner = [];
+            $.each(spells, function(spellID, val){
+                inner.push('[' + spellID + '] = "' + val.join('/') + '"');
+            });
+            arr.push('[' + itemID + '] = {' + inner.join(', ') + '}');
+        });
+    }
+    arr.push(''); // Pad the end so we have a comma on every line
+    console.log(arr.join(',\n'));
+}
+
+// One time, make a list of all the valid tabs
+current_tab = 0;
+// WARNING: potentially brittle, there is currently no better selector to find the expansion tabs bar instead of the comments bar
+//          there is no easy way to tell the difference between an actual expansion tab and "other" types of tabs (hard coded 9 expansions)
+//          (tabs are not consistently named)
+$tabs = $('ul.tabs').eq(0).find('li > a').slice(0,9);
+
+function main_loop() {
+    // expected 9 main tabs expansion tabs worth, with 1 or more pages each.
+    // Will start on latest expansion
+
+    var $nav       = $('.listview-band-top:visible .listview-nav');
+    var $next_page = $nav.find('a').eq(3);
+    var $next_tab  = $tabs.eq(current_tab + 1);
+
+    get_skill_levels();
+    // DEBUGGING:
+    //console.log('current-tab: ' + $tabs.eq(current_tab).text() + ' >>> ' + $nav.find('span').text());
+
+    // Try next page first
+    // REM: .data() does not necessarily update correctly
+    //      always use attr()
+    if ($next_page.attr('data-active') == 'yes') {
+        // DEBUGGING:
+        //console.log('Clicking next');
+        $next_page.click();
+        setTimeout(main_loop, 250); // Wait a moment to give it time to load
+        return;
+    } else if ($next_tab.length) {
+        current_tab++;
+        // DEBUGGING:
+        //console.log('Clicking: ' + $next_tab.text());
+        $next_tab.click();
+        setTimeout(main_loop, 1000); // Wait a second to give it plenty of time to load
+        return;
+    }
+    //console.log('done'); // DEBUGGING
+    print_skill_levels();
+}
+
+// [/paste]
+
+// run this to get all skill levels from this page's profession
+main_loop();
